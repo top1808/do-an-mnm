e15c1310358dee5e63a661e169d37d51fc9acda8
top <top180802@gmail.com>
2023-12-14 15:09:33 +0700

    them trang gio hang

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,17 @@ import Layout from "./layout/Layout";
 // import Product from "./pages/Product";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
-  // {
-  //   path: "/cart",
-  //   element: <div>cart</div>,
-  // },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
   // {
   //   path: "/product/:id",
   //   element: <Product />,
