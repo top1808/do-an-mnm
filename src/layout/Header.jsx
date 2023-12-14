ddb5688d881a55c399432269b40b5c7b1100f9cb
top <top180802@gmail.com>
2023-12-14 17:05:26 +0700

    fix chuyen trang khi click vao icon gio hang

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -18,15 +18,16 @@ const Header = () => {
     <div className="w-full bg-blue-400 p-4 text-white flex justify-between items-center">
 <a href="/" className="text-xl font-bold">APP</a>
       <div className="flex gap-12 items-center">
-        <button
+        <a
           type="button"
           className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white rounded-lgfocus:ring-4 focus:outline-none"
+          href="/cart"
         >
           <FaShoppingCart className="text-2xl" />
           <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900">
             {carts?.length}
           </div>
-        </button>
+        </a>
         {currentUser ? (
           <div className="text-white txt-base font-bold">
             {currentUser?.username} &nbsp;&nbsp;
